cafa68fe3699d85d9b84962f7fdd4fe4d72d5a5b
Patrick <patrickdiehl@lsu.edu>
2020-07-07 21:06:31 -0500

    Remove Docker file and move it to own repo

--- docker/generate.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-sudo podman build --tag docker.io/diehlpk/teaching:latest -f ./Dockerfile
-sudo podman login docker.io
-id=$(sudo podman inspect --format="{{.Id}}" docker.io/diehlpk/teaching:latest)
-sudo podman push $id docker://diehlpk/teaching:latest
